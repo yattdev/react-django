8c1c9c50167621a9f9a52bede93a380acff60976
yattdev <yattdeveloper@gmail.com>
2021-10-22 22:50:59 +0100

    Juste add 'x' to '.js' for src/App.js and now i've src/App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+// import logo from "./logo.svg";
+import "./App.css";
+import Setup from "./tutorial/3-conditional-rendering/setup/3-show-hide";
+
+function App() {
+  return (
+    <div className="container">
+      <Setup />
+    </div>
+  );
+}
+
+export default App;
