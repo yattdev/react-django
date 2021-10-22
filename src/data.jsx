59076c0661c8cbac4200840d5fe597b07dc2e9b5
yattdev <yattdeveloper@gmail.com>
2021-10-22 22:51:56 +0100

    Changed src/data content

--- src/data.jsx
+++ src/data.jsx
@@ -1,20 +1,14 @@
 export const data = [
   {
     id: 1,
-    title: "titre 1",
-    date: "Avant hier",
-    img: "https://www.themoviedb.org/t/p/w220_and_h330_face/53WqEWbwQQ3WsO6cOWkzNbym43.jpg",
+    name: "Alassane",
   },
   {
     id: 2,
-    title: "titre 2",
-    date: "Hier",
-    img: "https://www.themoviedb.org/t/p/w220_and_h330_face/5qQWZOSD1xn2lNge7kd4QaPTN3O.jpg",
+    name: "Tinamou",
   },
   {
     id: 3,
-    title: "titre 1",
-    date: "Aujourd'hui",
-    img: "https://www.themoviedb.org/t/p/w220_and_h330_face/9X7FovF5n8NQUHUPJYYfxRlF3yp.jpg",
+    name: "Aminata",
   },
 ];
